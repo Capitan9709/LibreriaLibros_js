f64e182d826afa440136629945c493353f71bda6
Luis Moya <86807831+Capitan9709@users.noreply.github.com>
2022-11-09 12:09:31 +0100

    Add files via upload

--- libros.js
+++ libros.js
@@ -1,40 +1,40 @@
-class Book {
-  constructor(tit, aut, gen) {
-    this.title = tit;
-    this.genre = gen;
-    this.author = aut;
-    this.read = false;
-    this.readDate = null;
-  }
+export class Book {
+    constructor(tit, aut, gen) {
+        this.title = tit;
+        this.genre = gen;
+        this.author = aut;
+        this.read = false;
+        this.readDate = null;
+    }
 }
 
-class BookList {
-  constructor() {
-    this.books = [];
-    this.currentBook = 0;
-  }
-
-  add(book) {
-    this.books.push(book);
-  }
-
-  finishCurrentBook() {
-    if (this.currentBook < this.books.length) {
-      this.books[this.currentBook].read = true;
-      this.books[this.currentBook].readDate = new Date();
-      this.currentBook++;
+export class BookList {
+    constructor() {
+        this.books = [];
+        this.currentBook = 0;
     }
-  }
 
-  get numberBooksRead() {
-    return this.books.filter((book) => book.read).length;
-  }
+    add(book) {
+        this.books.push(book);
+    }
 
-  get numberBooksNotReadYet() {
-    return this.books.length - this.numberBooksRead;
-  }
+    finishCurrentBook() {
+        if (this.currentBook < this.books.length) {
+        this.books[this.currentBook].read = true;
+        this.books[this.currentBook].readDate = new Date();
+        this.currentBook++;
+        }
+    }
 
-  get totalBooks() {
-    return this.books.length;
-  }
+    get numberBooksRead() {
+        return this.books.filter((book) => book.read).length;
+    }
+
+    get numberBooksNotReadYet() {
+        return this.books.length - this.numberBooksRead;
+    }
+
+    get totalBooks() {
+        return this.books.length;
+    }
 }
